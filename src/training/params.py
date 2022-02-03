29fa7524f4c5aea95251171598e8e11fbef0e994
Ross Wightman <rwightman@gmail.com>
2022-02-02 17:09:08 -0800

    Fix vit defaults for opt params

--- src/training/params.py
+++ src/training/params.py
@@ -4,7 +4,7 @@ import argparse
 def get_default_params(model_name):
     # Params from paper (https://arxiv.org/pdf/2103.00020.pdf)
     model_name = model_name.lower()
-    if model_name == "vit" in model_name:
+    if "vit" in model_name:
         return {"lr": 5.0e-4, "beta1": 0.9, "beta2": 0.98, "eps": 1.0e-6}
     else:
         return {"lr": 5.0e-4, "beta1": 0.9, "beta2": 0.999, "eps": 1.0e-8}
