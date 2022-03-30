2e8f3a48c389680f524a94b03f42ca544ad0a22c
Ross Wightman <rwightman@gmail.com>
2022-03-30 11:25:38 -0700

    Move ClipLoss to clip package, enable label caching for train

--- src/clip/__init__.py
+++ src/clip/__init__.py
@@ -1,4 +1,5 @@
 from .factory import create_model_and_transforms
+from .loss import ClipLoss
 from .model import CLIP, CLIPTextCfg, CLIPVisionCfg, convert_weights_to_fp16
 from .openai_clip import load_openai
 from .pretrained import list_pretrained, list_pretrained_tag_models, list_pretrained_model_tags,\
