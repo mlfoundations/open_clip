08a5db3006b9b160d3af1d8726d90ba93ab57da6
Romain Beaumont <romain.rom1@gmail.com>
2022-04-07 00:06:01 +0200

    [DO NOT MERGE] broken commit to test the ci

--- src/open_clip/__init__.py
+++ src/open_clip/__init__.py
@@ -1,4 +1,4 @@
-from .factory import list_models, create_model, create_model_and_transforms, add_model_config
+from .factory import list_models, create_model, add_model_config
 from .loss import ClipLoss
 from .model import CLIP, CLIPTextCfg, CLIPVisionCfg, convert_weights_to_fp16, trace_model
 from .openai import load_openai_model, list_openai_models
