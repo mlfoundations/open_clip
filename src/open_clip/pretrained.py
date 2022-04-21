045a96c973db61bbab51424a4a2080f594211334
Ross Wightman <rwightman@gmail.com>
2022-04-21 10:03:08 -0700

    Fix case when loading pretrained local checkpoints via --pretrained arg

--- src/open_clip/pretrained.py
+++ src/open_clip/pretrained.py
@@ -103,6 +103,7 @@ def get_pretrained_url(model: str, tag: str):
     if model not in _PRETRAINED:
         return ''
     model_pretrained = _PRETRAINED[model]
+    tag = tag.lower()
     if tag not in model_pretrained:
         return ''
     return model_pretrained[tag]
