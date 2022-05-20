7ddf1a2c34cb843a64613809d096bfcb76ee61b5
iejMac <kilianmaciej6@gmail.com>
2022-05-20 19:19:11 +0200

    Building casual attention mask

--- src/open_clip/hf_model.py
+++ src/open_clip/hf_model.py
@@ -26,6 +26,7 @@ class DummyPooler(nn.Module):
     "Fetches first of output hidden state"
 
     def __init__(self):
+        super().__init__()
         pass
     
     def forward(self, x):
@@ -48,10 +49,22 @@ class PreTrainedTextEncoder(nn.Module):
     def __init__(
             self, 
             model_name_or_path:str,
-            embedding_dim:int,
+
+            context_length:int = 77,
+            vocab_size:int = 49408,
+            width:int = 512,
+            heads:int = 8,
+            layers:int = 12,
+            output_dim:int = 512,
             pooler_type:str=None,
             proj:str=None):
         super().__init__()
+
+        self.context_length = context_length
+        self.vocab_size = vocab_size
+        self.width = width
+        self.output_dim = output_dim
+
         if transformers is None:
             raise RuntimeError("Please `pip install transformers` to use pre-trained HuggingFace models")
         self.config = AutoConfig.from_pretrained(model_name_or_path)
@@ -61,17 +74,31 @@ class PreTrainedTextEncoder(nn.Module):
         d_model = self.config.hidden_size # TODO: get d_model from config
         # different models can have different names for it
         # ?? do we use separate classes for different archs or handle it with helper funcs  
-        if (d_model == embedding_dim) and (proj is None): # do we always need a proj?
+        if (d_model == width) and (proj is None): # do we always need a proj?
             self.proj = nn.Identity()
         elif proj == 'linear':
-            self.proj == nn.Linear(d_model, embedding_dim, bias=False)
+            self.proj == nn.Linear(d_model, width, bias=False)
         elif proj == 'mlp':
             # TODO: add me
             pass
 
+        self.register_buffer('attn_mask', self.build_attention_mask(), persistent=False)
+
+    def build_attention_mask(self):
+        # lazily create causal attention mask, with full attention between the vision tokens
+        # pytorch uses additive attention mask; fill with -inf
+        mask = torch.empty(self.context_length, self.context_length)
+        mask.fill_(float("-inf"))
+        mask.triu_(1)  # zero out the lower diagonal
+        return mask
+
     def forward(self, x:TensorType) -> TensorType:
         # TODO: add attention mask?
-        out = self.transformer(input_ids=x)
+        pad = torch.full((x.shape[0], self.context_length), self.config.pad_token_id)
+        pad[:, :x.shape[1]] = x
+        x = pad
+
+        out = self.transformer(input_ids=x, attention_mask=self.attn_mask[:x.shape[0]])
         pooled_out = self.pooler(out)
 
         return self.proj(pooled_out)
