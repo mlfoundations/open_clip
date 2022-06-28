5a45e5ff296bc721ea91cc7330e9465d0efd3f5a
penfever <penfever@gmail.com>
2022-06-28 08:22:36 -0400

    avoid find unused when not coca

--- src/training/main.py
+++ src/training/main.py
@@ -152,7 +152,11 @@ def main():
         if args.ddp_static_graph:
             # this doesn't exist in older PyTorch, arg only added if enabled
             ddp_args['static_graph'] = True
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[device], find_unused_parameters=True, **ddp_args)
+        if args.model == "coca":
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[device], find_unused_parameters=True, **ddp_args)
+        else:
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[device], **ddp_args)
+
 
     # create optimizer and scaler
     optimizer = None
