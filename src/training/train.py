9a4d38e07921c640f95b722ef922ba579b870b8c
Romain Beaumont <romain.rom1@gmail.com>
2022-08-10 00:03:38 +0200

    Introduce norm_gradient_clip parameter.

--- src/training/train.py
+++ src/training/train.py
@@ -86,13 +86,20 @@ def train_one_epoch(model, data, epoch, optimizer, scaler, scheduler, args, tb_w
             if args.horovod:
                 optimizer.synchronize()
                 scaler.unscale_(optimizer)
+                if args.norm_gradient_clip is not None:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.norm_gradient_clip, norm_type=2.0)
                 with optimizer.skip_synchronize():
                     scaler.step(optimizer)
             else:
+                if args.norm_gradient_clip is not None:
+                    scaler.unscale_(optimizer)
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.norm_gradient_clip, norm_type=2.0)
                 scaler.step(optimizer)
             scaler.update()
         else:
             total_loss.backward()
+            if args.norm_gradient_clip is not None:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.norm_gradient_clip, norm_type=2.0)
             optimizer.step()
 
         # Note: we clamp to 4.6052 = ln(100), as in the original paper.
