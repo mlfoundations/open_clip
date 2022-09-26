0cd412c707876c2439a14085bc7e12558967d85e
Romain Beaumont <romain.rom1@gmail.com>
2022-09-26 22:30:20 +0200

    Add jit=True to check we don't break torchscript

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -7,7 +7,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 def test_inference():
-    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32')
+    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32', jit=True)
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
