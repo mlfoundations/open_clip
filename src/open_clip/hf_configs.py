bb924a8715bc9fe92728c4980f3ecaab3bd9ee31
Romain Beaumont <romain.rom1@gmail.com>
2022-11-10 17:09:44 +0100

    Add a configuration using xlm roberta and vit b/32 for multilingual clip (#216)
    * Add a configuration using xlm roberta and vit b/32 for multilingual clip
    
    https://huggingface.co/docs/transformers/model_doc/xlm-roberta
    
    * Update xlm-roberta-base-ViT-B-32.json
    
    * Update hf_configs.py
    
    * Update test_hf_model.py
    
    * Update test_hf_model.py
    
    * Update hf_configs.py
    
    * Update xlm-roberta-base-ViT-B-32.json

--- src/open_clip/hf_configs.py
+++ src/open_clip/hf_configs.py
@@ -9,5 +9,16 @@ arch_dict = {
       "layers": "num_hidden_layers",
     },
     "pooler": "mean_pooler",
+  },
+  # https://huggingface.co/docs/transformers/model_doc/xlm-roberta#transformers.XLMRobertaConfig
+  "xlm-roberta": {
+    "config_names": {
+      "context_length": "max_position_embeddings",
+      "vocab_size": "vocab_size",
+      "width": "hidden_size",
+      "heads": "num_attention_heads",
+      "layers": "num_hidden_layers",
+    },
+    "pooler": "mean_pooler",
   }
 }
