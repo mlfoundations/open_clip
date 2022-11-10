bd7a3776472d12a79c35eab61c89a905ce845662
iejmac <iejmac@gpu-st-p4d-24xlarge-4.hpc-1click-sandbox.pcluster>
2022-11-10 01:07:11 +0000

    add get-tokenizer to README + update exmaple

--- src/open_clip/__init__.py
+++ src/open_clip/__init__.py
@@ -1,5 +1,5 @@
 from .constants import OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
-from .factory import create_model, create_model_and_transforms, create_model_from_pretrained
+from .factory import create_model, create_model_and_transforms, create_model_from_pretrained, get_tokenizer
 from .factory import list_models, add_model_config, get_model_config, load_checkpoint
 from .loss import ClipLoss
 from .model import CLIP, CustomTextCLIP, CLIPTextCfg, CLIPVisionCfg,\
--- src/training/main.py
+++ src/training/main.py
@@ -24,8 +24,7 @@ try:
 except ImportError:
     hvd = None
 
-from open_clip import create_model_and_transforms, trace_model
-from open_clip.factory import get_tokenizer
+from open_clip import create_model_and_transforms, trace_model, get_tokenizer
 from training.data import get_data
 from training.distributed import is_master, init_distributed_device, world_info_from_env
 from training.logger import setup_logging
