4c710193d9d53898c945a63ec0e72422b574a902
iejmac <iejmac@gpu-st-p4d-24xlarge-4.hpc-1click-sandbox.pcluster>
2022-11-10 00:50:09 +0000

    fix test

--- tests/test_inference_simple.py
+++ tests/test_inference_simple.py
@@ -1,7 +1,7 @@
 
 import torch
 from PIL import Image
-from open_clip import tokenizer
+from open_clip.factory import get_tokenizer
 import pytest
 import open_clip
 import os
@@ -10,11 +10,12 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 @pytest.mark.parametrize("jit", [False, True])
 def test_inference(jit):
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32', jit=jit)
+    tokenizer = get_tokenizer('ViT-B-32-quickgelu')
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
     image = preprocess(Image.open(current_dir + "/../docs/CLIP.png")).unsqueeze(0)
-    text = model.tokenizer(["a diagram", "a dog", "a cat"])
+    text = tokenizer(["a diagram", "a dog", "a cat"])
 
     with torch.no_grad():
         image_features = model.encode_image(image)
