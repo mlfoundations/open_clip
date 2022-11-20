fea9dee401861eeefccf03b1100b05f5c3bb9ebf
iejMac <kilianmaciej6@gmail.com>
2022-11-20 05:46:38 +0000

    populate configs with some values

--- src/open_clip/hf_configs.py
+++ src/open_clip/hf_configs.py
@@ -1,5 +1,6 @@
 # HF architecture dict:
 arch_dict = {
+  # https://huggingface.co/docs/transformers/model_doc/roberta#roberta
   "roberta": {
     "config_names": {
       "context_length": "max_position_embeddings",
@@ -21,8 +22,14 @@ arch_dict = {
     },
     "pooler": "mean_pooler",
   },
+  # https://huggingface.co/docs/transformers/model_doc/mt5#mt5
   "mt5": {
     "config_names": {
+        "context_length": "relative_attention_max_distance", # very unsure of this, couldn't find anything better
+        "vocab_size": "vocab_size",
+        "width": "d_model",
+        "heads": "num_heads",
+        "layers": "num_layers",
     }
     "pooler": "mean_pooler",
   },
