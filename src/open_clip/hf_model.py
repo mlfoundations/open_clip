b61eea1186313d9bfb5781104ada5d193b05db34
iejMac <kilianmaciej6@gmail.com>
2022-11-22 22:54:00 +0000

    fix

--- src/open_clip/hf_model.py
+++ src/open_clip/hf_model.py
@@ -85,7 +85,7 @@ class HFTextEncoder(nn.Module):
             raise RuntimeError("Please `pip install transformers` to use pre-trained HuggingFace models")
         if config is None:
             self.config = AutoConfig.from_pretrained(model_name_or_path)
-            create_func, model_args = AutoModel.from_pretrained, model_name_or_path if pretrained else AutoModel.from_config, self.config
+            create_func, model_args = (AutoModel.from_pretrained, model_name_or_path) if pretrained else (AutoModel.from_config, self.config)
             # TODO: do all model configs have this attribute? PretrainedConfig does so yes??
             if hasattr(self.config, "is_encoder_decoder") and self.config.is_encoder_decoder:
                 self.transformer = create_func(model_args)
