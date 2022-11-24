5adea0b3ed3c1e1d1ec6223eb5046a57e02fa870
Romain Beaumont <romain.rom1@gmail.com>
2022-11-24 03:48:32 +0100

    Add args.log_every_n_steps

--- src/training/train.py
+++ src/training/train.py
@@ -112,7 +112,7 @@ def train_one_epoch(model, data, epoch, optimizer, scaler, scheduler, args, tb_w
         batch_time_m.update(time.time() - end)
         end = time.time()
         batch_count = i + 1
-        if is_master(args) and (i % 100 == 0 or batch_count == num_batches_per_epoch):
+        if is_master(args) and (i % args.log_every_n_steps == 0 or batch_count == num_batches_per_epoch):
             batch_size = len(images)
             num_samples = batch_count * batch_size * args.world_size
             samples_per_epoch = dataloader.num_samples
