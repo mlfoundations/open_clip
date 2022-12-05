59503df4c8a295a90657621ea3025d33db788be5
gpucce <g.puccetti92@gmail.com>
2022-12-05 16:29:27 +0100

    add coca funcs to init

--- src/open_clip/__init__.py
+++ src/open_clip/__init__.py
@@ -4,6 +4,7 @@ from .factory import list_models, add_model_config, get_model_config, load_check
 from .loss import ClipLoss, CoCaLoss
 from .model import CLIP, CustomTextCLIP, CLIPTextCfg, CLIPVisionCfg,\
     convert_weights_to_lp, convert_weights_to_fp16, trace_model, get_cast_dtype
+from .coca_model import CoCa, CoCaCfg
 from .openai import load_openai_model, list_openai_models
 from .pretrained import list_pretrained, list_pretrained_models_by_tag, list_pretrained_tags_by_model,\
     get_pretrained_url, download_pretrained_from_url, is_pretrained_cfg, get_pretrained_cfg, download_pretrained
