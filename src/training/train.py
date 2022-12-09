d547017d0459884ac60d5dae4f2efbce4c5858bd
gpucce <g.puccetti@gmail.com>
2022-12-09 16:40:32 +0900

    remove print

--- src/training/train.py
+++ src/training/train.py
@@ -315,6 +315,4 @@ def maybe_compute_generative_loss(model_out):
     if len(model_out) > 3:
         token_logits = model_out[2]
         token_labels = model_out[3]
-        print("TOKEN_LOGITS", token_logits.shape)
-        print("TOKEN_LABELS", token_labels.shape)
         return F.cross_entropy(token_logits.permute(0, 2, 1), token_labels)
