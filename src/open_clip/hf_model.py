fix cls masking

src/open_clip/hf_model.py
@@ -138,8 +138,9 @@ class HFTextEncoder(nn.Module):
         projected = self.proj(pooled_out)
 
         if output_tokens:
+            seq_len = out.last_hidden_state.shape[1]
             tokens = self.proj(
-                out.last_hidden_state[:, self.pooler.cls_token_position + 1:, :] 
+                out.last_hidden_state[:, torch.arange(seq_len) != self.pooler.cls_token_position, :] 
                 if type(self.pooler) == ClsPooler 
                 else out.last_hidden_state
             )