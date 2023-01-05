5d331d972e85b59e7189720959b0a61b079e8c8d
lukeum <coronalbayes@protonmail.com>
2023-01-04 21:37:54 -0600

    rename variables

--- tests/test_TextTextCLIP.py
+++ tests/test_TextTextCLIP.py
@@ -16,25 +16,22 @@ def test_inference_simple(model_type, pretrained=None):
     model, _, preprocess = open_clip.create_model_and_transforms(model_type, pretrained=pretrained, jit=False, text_to_text=True)
     tokenizer = get_tokenizer(model_type)
 
-    doc = tokenizer(['this', 'is', 'a', 'document'])
-    query = tokenizer(['this', 'is', 'a', 'summary'])
+    text_a = tokenizer(['this', 'is', 'a', 'document'])
+    text_b = tokenizer(['this', 'is', 'a', 'summary'])
 
     with torch.no_grad():
-        doc_features = model.encode_doc(doc)
-        query_features = model.encode_query(query)
+        text_a_features = model.encode_text_a(text_a)
+        text_b_features = model.encode_text_b(text_b)
     
-    print(doc_features.shape)
-    print(query_features.shape)
+    print(text_a_features.shape)
+    print(text_b_features.shape)
 
-    text_probs = (100.0 * doc_features @ query_features.T).softmax(dim=-1)
+    text_probs = (100.0 * text_a_features @ text_b_features.T).softmax(dim=-1)
     print(text_probs)
 
 '''    
 if __name__ == "__main__":
-    
-    #open_clip.factory._rescan_model_configs()
-    #print(open_clip.factory._MODEL_CONFIG_PATHS)
-    #test_inference_simple("roberta-roberta",'')
+
 
     main([
     '--save-frequency', '1',
