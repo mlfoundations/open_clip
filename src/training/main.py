211f2a78f3af706f46e6f25e28d109dbbc6ba0a3
Maciej Kilian <kilianmaciej6@gmail.com>
2023-01-11 00:02:18 +0100

    wandb: don't set autoresume if resume arg isn't set (#347)
    * wandb: don't set autoresume if resume arg isn't set
    
    * only on latest

--- src/training/main.py
+++ src/training/main.py
@@ -295,7 +295,7 @@ def main(args):
             id=args.name,
             notes=args.wandb_notes,
             tags=[],
-            resume='auto',
+            resume='auto' if args.resume == "latest" else None,
             config=vars(args),
         )
         if args.debug:
