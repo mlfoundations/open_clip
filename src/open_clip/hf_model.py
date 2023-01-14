17aef53e9d39fb68a4c111dfa765d9b98bcf0e19
Giovanni Puccetti <g.puccetti92@gmail.com>
2023-01-14 11:28:17 +0100

    make hf simpler too

--- src/open_clip/hf_model.py
+++ src/open_clip/hf_model.py
@@ -92,11 +92,8 @@ class HFTextEncoder(nn.Module):
             pooler_type: str = None,
             proj: str = None,
             pretrained: bool = True,
-            output_tokens: bool = False
         ):
         super().__init__()
-        if output_tokens:
-            self.output_tokens = output_tokens
         self.output_dim = output_dim
 
         # TODO: find better way to get this information
@@ -136,21 +133,22 @@ class HFTextEncoder(nn.Module):
             )
 
     def forward(self, x: TensorType):
+        pooled, _ = self.encode_with_tokens(x)
+        return pooled
+    
+    def encode_with_tokens(self, x: TensorType):
         attn_mask = (x != self.config.pad_token_id).long()
         out = self.transformer(input_ids=x, attention_mask=attn_mask)
         pooled_out = self.pooler(out, attn_mask)
         projected = self.proj(pooled_out)
 
-        if hasattr(self, "output_tokens"):
-            seq_len = out.last_hidden_state.shape[1]
-            tokens = (
-                out.last_hidden_state[:, torch.arange(seq_len) != self.pooler.cls_token_position, :] 
-                if type(self.pooler) == ClsPooler 
-                else out.last_hidden_state
-            )
-            return (projected, tokens)
-
-        return projected
+        seq_len = out.last_hidden_state.shape[1]
+        tokens = (
+            out.last_hidden_state[:, torch.arange(seq_len) != self.pooler.cls_token_position, :] 
+            if type(self.pooler) == ClsPooler 
+            else out.last_hidden_state
+        )
+        return projected, tokens
 
     def lock(self, unlocked_layers: int = 0, freeze_layer_norm: bool = True):
         if not unlocked_layers:  # full freezing
