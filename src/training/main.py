434db0ed569fe14c535a27820c0c06819c96717e
Ross Wightman <rwightman@gmail.com>
2023-01-30 09:16:46 -0800

    Fix #394, typo in project date string

--- src/training/main.py
+++ src/training/main.py
@@ -85,7 +85,7 @@ def main(args):
     if args.name is None:
         # sanitize model name for filesystem / uri use, easier if we don't use / in name as a rule?
         model_name_safe = args.model.replace('/', '-')
-        date_str = datetime.now().strftime("%Y_%m_%d`-%H_%M_%S")
+        date_str = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
         if args.distributed:
             # sync date_str from master to all ranks
             date_str = broadcast_object(args, date_str)
