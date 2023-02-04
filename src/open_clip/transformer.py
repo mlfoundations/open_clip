897be628571692caed7e92af5b705c33d5c855cb
Phil Wang <lucidrains@gmail.com>
2023-02-03 18:48:27 -0800

    add audio spectrogram transformer that utilizes vision transformer (which it is, as the premise is to change the waveform to a 2d "image" and patchify and attend)

--- src/open_clip/transformer.py
+++ src/open_clip/transformer.py
@@ -7,6 +7,8 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.checkpoint import checkpoint
 
+from torchaudio.transforms import Spectrogram, TimeStretch, FrequencyMasking, TimeMasking
+
 from .utils import to_2tuple
 
 
@@ -340,7 +342,8 @@ class VisionTransformer(nn.Module):
             patch_dropout: float = 0.,
             act_layer: Callable = nn.GELU,
             norm_layer: Callable = LayerNorm,
-            output_tokens: bool = False
+            output_tokens: bool = False,
+            channels: int = 3
     ):
         super().__init__()
         self.output_tokens = output_tokens
@@ -348,7 +351,7 @@ class VisionTransformer(nn.Module):
         self.patch_size = to_2tuple(patch_size)
         self.grid_size = (self.image_size[0] // self.patch_size[0], self.image_size[1] // self.patch_size[1])
         self.output_dim = output_dim
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
 
         scale = width ** -0.5
         self.class_embedding = nn.Parameter(scale * torch.randn(width))
@@ -450,7 +453,7 @@ class VisionTransformer(nn.Module):
         x = torch.cat(
             [self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device),
              x], dim=1)  # shape = [*, grid ** 2 + 1, width]
-        x = x + self.positional_embedding.to(x.dtype)
+        x = x + self.positional_embedding[:x.shape[1]].to(x.dtype)
 
         # a patch_dropout of 0. would mean it is disabled and this function would do nothing but return what was passed in
         x = self.patch_dropout(x)
@@ -475,6 +478,112 @@ class VisionTransformer(nn.Module):
             return pooled, tokens
         
         return pooled
+
+
+class AudioSpectrogramTransformer(nn.Module):
+    def __init__(
+            self,
+            image_size: int,
+            patch_size: int,
+            width: int,
+            layers: int,
+            heads: int,
+            mlp_ratio: float,
+            ls_init_value: float = None,
+            global_average_pool: bool = False,
+            attentional_pool: bool = False,
+            n_queries: int = 256,
+            attn_pooler_heads: int = 8,
+            output_dim: int = 512,
+            patch_dropout: float = 0.,
+            act_layer: Callable = nn.GELU,
+            norm_layer: Callable = LayerNorm,
+            output_tokens: bool = False,
+            spec_n_fft: int = 128,
+            spec_power: int = 2,
+            spec_win_length: int = 24,
+            spec_hop_length: Optional[int] = None,
+            spec_pad: int = 0,
+            spec_center: bool = True,
+            spec_pad_mode: str = 'reflect',
+            aug_stretch_factor: float = 0.8,
+            aug_freq_mask: int = 80,
+            aug_time_mask: int = 80,
+    ):
+        super().__init__()
+
+        self.patch_size = to_2tuple(patch_size)
+
+        self.spec = Spectrogram(
+            n_fft=spec_n_fft,
+            power=spec_power,
+            win_length=spec_win_length,
+            hop_length=spec_hop_length,
+            pad=spec_pad,
+            center=spec_center,
+            pad_mode=spec_pad_mode
+        )
+
+        # spec augment - https://arxiv.org/abs/1904.08779
+
+        self.aug = torch.nn.Sequential(
+            TimeStretch(aug_stretch_factor, fixed_rate=True),
+            FrequencyMasking(freq_mask_param=aug_freq_mask),
+            TimeMasking(time_mask_param=aug_time_mask),
+        )
+
+        self.vit = VisionTransformer(
+            image_size=image_size,
+            patch_size=patch_size,
+            width=width,
+            layers=layers,
+            heads=heads,
+            mlp_ratio=mlp_ratio,
+            ls_init_value=ls_init_value,
+            global_average_pool=global_average_pool,
+            attentional_pool=attentional_pool,
+            n_queries=n_queries,
+            attn_pooler_heads=attn_pooler_heads,
+            output_dim=output_dim,
+            patch_dropout=patch_dropout,
+            act_layer=act_layer,
+            norm_layer=norm_layer,
+            output_tokens=output_tokens,
+            channels=1
+        )
+
+    def lock(self, unlocked_groups=0, freeze_bn_stats=False):
+        self.vit.lock(unlocked_groups=unlocked_groups, freeze_bn_stats=freeze_bn_stats)
+
+    def init_parameters(self):
+        self.vit.init_parameters()
+
+    @torch.jit.ignore
+    def set_grad_checkpointing(self, enable=True):
+        self.vit.set_grad_checkpointing(enable=enable)
+
+    def forward(self, x: torch.Tensor):
+        x = self.spec(x)
+
+        if self.training:
+            x = self.aug(x)
+
+        # automatically crop if audio does not yield a 2d spectrogram that is divisible by patch sizes
+
+        height, width = x.shape[-2:]
+        patch_height, patch_width = self.patch_size
+
+        rounded_height = height // patch_height * patch_height
+        rounded_width = width // patch_width * patch_width
+
+        if (height, width) != (rounded_height, rounded_width):
+            print(f'spectrogram yielded shape of {(height, width)}, but had to be cropped to {(rounded_height, rounded_width)} to be patchified for transformer')
+
+        x = x[..., None, :rounded_height, :rounded_width]
+
+        # pass maybe cropped spectrogram to vit
+
+        return self.vit(x)
 
 
 class TextTransformer(nn.Module):
