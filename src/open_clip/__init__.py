252c389ca71a6f910d76bba3e7969bf10e0ed2fa
Ross Wightman <rwightman@users.noreply.github.com>
2023-02-10 20:30:50 -0800

    Add push to Hugging Face Hub helpers (#414)
    * Add push_to_hf_hub functionality to make it easy to push trained models to hf.co
    
    * PEP ordering of open_clip/__init__, EOF CR for same and push_to_hub

--- src/open_clip/__init__.py
+++ src/open_clip/__init__.py
@@ -1,12 +1,13 @@
+from .coca_model import CoCa
 from .constants import OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
 from .factory import create_model, create_model_and_transforms, create_model_from_pretrained, get_tokenizer, create_loss
 from .factory import list_models, add_model_config, get_model_config, load_checkpoint
 from .loss import ClipLoss, DistillClipLoss, CoCaLoss
-from .model import CLIP, CustomTextCLIP, CLIPTextCfg, CLIPVisionCfg,\
+from .model import CLIP, CustomTextCLIP, CLIPTextCfg, CLIPVisionCfg, \
     convert_weights_to_lp, convert_weights_to_fp16, trace_model, get_cast_dtype
-from .coca_model import CoCa
 from .openai import load_openai_model, list_openai_models
-from .pretrained import list_pretrained, list_pretrained_models_by_tag, list_pretrained_tags_by_model,\
+from .pretrained import list_pretrained, list_pretrained_models_by_tag, list_pretrained_tags_by_model, \
     get_pretrained_url, download_pretrained_from_url, is_pretrained_cfg, get_pretrained_cfg, download_pretrained
+from .push_to_hf_hub import push_pretrained_to_hf_hub, push_to_hf_hub
 from .tokenizer import SimpleTokenizer, tokenize, decode
 from .transform import image_transform, AugmentationCfg
