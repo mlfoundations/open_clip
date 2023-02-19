c4046a84a2730f062183b129070276dd44d799cd
Romain Beaumont <romain.rom1@gmail.com>
2023-02-19 16:56:49 +0100

    Update stability_example.sh

--- docs/script_examples/stability_example.sh
+++ docs/script_examples/stability_example.sh
@@ -12,9 +12,22 @@
 module load openmpi
 module load cuda/11.7
 
-export PYTHONFAULTHANDLER=1
 export MASTER_ADDR=`hostname`
 export MASTER_PORT=12802
+export NCCL_PROTO=simple
+export FI_EFA_FORK_SAFE=1
+export FI_LOG_LEVEL=1
+export FI_EFA_USE_DEVICE_RDMA=1
+export NCCL_DEBUG=info
+
+export PYTHONFAULTHANDLER=1
+
+export CUDA_LAUNCH_BLOCKING=0
+export OMPI_MCA_mtl_base_verbose=1
+export FI_EFA_ENABLE_SHM_TRANSFER=0
+export FI_PROVIDER=efa
+export FI_EFA_TX_MIN_CREDITS=64
+export NCCL_TREE_THRESHOLD=0
 
 cd /admin/home-mitchellw/open_clip/src
 export PYTHONPATH="$PYTHONPATH:/admin/home-mitchellw/open_clip/src"
