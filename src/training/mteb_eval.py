330d4d9e70867e1e398db6782c6c3927757545bd
lingjzhu <jianz@ip-172-31-44-155.ec2.internal>
2023-06-20 05:26:32 +0000

    MTEB benchmark

--- src/training/mteb_eval.py
+++ src/training/mteb_eval.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import torch
 import torch.nn as nn
 from mteb import MTEB
@@ -17,7 +18,7 @@ class CLANP_For_MTEB(nn.Module):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model.to(self.device)
 
-    def encode(self, sentences, batch_size=32, show_progress_bar=True, **kwargs):
+    def encode(self, sentences, batch_size=128, show_progress_bar=True, **kwargs):
         """ Returns a list of embeddings for the given sentences.
         Args:
             sentences (`List[str]`): List of sentences to encode
@@ -34,10 +35,11 @@ class CLANP_For_MTEB(nn.Module):
             features = self.tokenizer(sentences_batch)
             features = features.to(self.device)
             with torch.no_grad():
-                text_features = self.model.encode_text(features)
-            embeddings = [f for f in text_features.detach().cpu().numpy()]
+                embeddings = self.model.encode_text(features)
+            
+            all_embeddings.append(embeddings.detach())
 
-            all_embeddings.extend(embeddings)
+        all_embeddings = torch.cat(all_embeddings,0)
 
         return all_embeddings
 
@@ -47,7 +49,7 @@ class CLANP_For_MTEB(nn.Module):
 def run_mteb(args):
 
     model = CLANP_For_MTEB(args)
-    evaluation = MTEB(tasks=['STS22']) #task_types=['Clustering','retrieval']
+    evaluation = MTEB(tasks=['MSMARCO']) #task_types=['Clustering','retrieval']
     results = evaluation.run(model, eval_splits=["test"], output_folder=args.mteb_output)
 
 
@@ -58,5 +60,6 @@ if __name__ == "__main__":
 
     # /fsx/home-jianz/logs/2023_05_11-16_04_50-model_Siamese-pythia-410m-lr_0.0001-b_1024-j_6-p_amp_bfloat16/checkpoints/epoch_1.pt
     # srun --account=laion --partition=g40 nodes=1 --ntasks-per-node=1 --cpus-per-task=4 --job-name=MTEB --pty bash -i
-    # python3 src/training/mteb_eval.py --model="Siamese-pythia-410m" --pretrained /fsx/home-jianz/logs/2023_05_11-16_04_50-model_Siamese-pythia-410m-lr_0.0001-b_1024-j_6-p_amp_bfloat16/checkpoints/epoch_1.pt --mteb-output /fsx/home-jianz/logs/mteb --context-length=77
+    # srun --account laion --partition=g40 --gpus=1 --cpus-per-gpu=4 --job-name=pty --pty bash -i
+    # python3 src/training/mteb_eval.py --model="Siamese-xlm-roberta-large" --pretrained /fsx/home-jianz/logs/2023_05_25-09_21_35-model_Siamese-xlm-roberta-large-lr_1e-05-b_256-j_10-p_amp_bfloat16/checkpoints/epoch_latest.pt --mteb-output /fsx/home-jianz/logs/mteb --context-length=128
     
