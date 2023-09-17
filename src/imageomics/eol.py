36ce0bd0dcbcc8a53415798a22c50d5a9386deae
Samuel Stevens <samuel.robert.stevens@gmail.com>
2023-09-17 17:04:40 -0400

    Dependencies for make_evobio10m.py

--- src/imageomics/eol.py
+++ src/imageomics/eol.py
@@ -4,6 +4,8 @@ import re
 
 from tqdm import tqdm
 
+from . import naming
+
 eol_filename_pattern = re.compile(r"(\d+)_(\d+)_eol.*jpg")
 
 
@@ -34,26 +36,23 @@ class VernacularName:
 
 
 class VernacularNameLookup:
-    vernacularnames_csv = "data/eol/vernacularnames.csv"
-
-    def __init__(self, vernacularnames_csv=None):
-        if vernacularnames_csv is not None:
-            self.vernacularnames_csv = vernacularnames_csv
-
+    def __init__(self, vernacularnames_csv="data/eol/vernacularnames.csv"):
         self.lookup = {}
 
-        with open(self.vernacularnames_csv) as fd:
+        with open(vernacularnames_csv) as fd:
             reader = csv.reader(fd)
             next(reader)  # skip header row
             for row, (page_id, canonical, common, lang, _, _, preferred) in enumerate(
-                tqdm(reader, desc=self.vernacularnames_csv)
+                tqdm(reader, desc=vernacularnames_csv)
             ):
                 if lang != "eng":  # only keep english
                     continue
 
                 page_id = int(page_id)
                 preferred = bool(preferred)
-                name = VernacularName(common, canonical, preferred)
+                name = VernacularName(
+                    naming.strip_html(common), naming.strip_html(canonical), preferred
+                )
 
                 if page_id not in self.lookup:
                     self.lookup[page_id] = name
@@ -77,3 +76,183 @@ class VernacularNameLookup:
 
     def __iter__(self):
         yield from iter(self.lookup)
+
+
+def parse_all_hierarchies_in_graph(path):  # dict[str, naming.Taxon]
+    """Parses the all_hierarchies_in_graph.csv file.
+
+    Returns a dictionary from page_id to a Taxon"""
+    dct = {}
+    # scientific names
+    with open(path) as fd:
+        reader = csv.reader(fd)
+        next(reader)  # skip header row
+        for page_id, tier_chain, rank_chain, _ in tqdm(reader, desc=path):
+            king = ""
+            phyl = ""
+            cls = ""
+            ord = ""
+            fam = ""
+            genus = ""
+            species = ""
+
+            for tier, rank in zip(tier_chain.split("->"), rank_chain.split("->")):
+                if rank == "kingdom" and not king:
+                    king = tier
+                if rank == "phylum" and not phyl:
+                    phyl = tier
+
+                # There are lots of different "class" levels in the chain, and some of
+                # them are not between phyl and ord, so we need this extra check
+                if rank == "class" and not cls and phyl and not ord:
+                    cls = tier
+
+                if rank == "order" and not ord:
+                    ord = tier
+                if rank == "family" and not fam:
+                    fam = tier
+                if rank == "genus" and not genus:
+                    genus = tier
+                if rank == "species" and not species:
+                    species = tier
+
+            taxon = naming.Taxon(king, phyl, cls, ord, fam, genus, species)
+            page_id = int(page_id)
+
+            if page_id not in dct:
+                dct[page_id] = taxon
+
+    return dct
+
+
+class EolNameLookup(naming.NameLookup):
+    def __init__(
+        self,
+        *,
+        hierarchies_path="/fs/scratch/PAS2136/eol/data/interim/all_hierarchies_in_graph.csv",
+        media_cargo_archive_map_csv="/fs/ess/PAS2136/eol/data/interim/media_cargo_archive_map.csv",
+        pages_csv="data/eol/trait_bank/pages.csv",
+        provider_ids_csv="data/eol/provider_ids.csv",
+        scraped_page_ids_csv="data/eol/scraped_page_ids.csv",
+        taxon_tab="data/eol/dh21/taxon.tab",
+        vernacularnames_csv="data/eol/vernacularnames.csv",
+    ):
+        self._common = {}
+        self._taxonomic = {}
+        self._scientific = {}
+        self._tagged = {}
+
+        # scientific names
+        with open(provider_ids_csv) as fd:
+            reader = csv.DictReader(fd)
+            for row in tqdm(reader, desc=provider_ids_csv):
+                if not row["page_id"]:
+                    continue
+                page_id = int(row["page_id"])
+
+                name = naming.strip_html(row["preferred_canonical_for_page"])
+                if not name:
+                    continue
+
+                if page_id not in self._scientific:
+                    self._scientific[page_id] = name
+
+        # scientific names
+        with open(taxon_tab) as fd:
+            reader = csv.DictReader(fd, delimiter="\t")
+            for row in tqdm(reader, desc=taxon_tab):
+                if not row["eolID"]:
+                    continue
+                page_id = int(row["eolID"])
+
+                name = naming.strip_html(row["scientificName"])
+                if not name:
+                    continue
+
+                if page_id not in self._scientific:
+                    self._scientific[page_id] = name
+
+        # scientific names
+        with open(pages_csv) as fd:
+            reader = csv.DictReader(fd)
+            for row in tqdm(reader, desc=pages_csv):
+                if not row["page_id"]:
+                    continue
+                page_id = int(row["page_id"])
+
+                name = naming.strip_html(row["canonical"])
+                if not name:
+                    continue
+
+                if page_id not in self._scientific:
+                    self._scientific[page_id] = name
+
+        # scientific and common names
+        vernacular_names = VernacularNameLookup(vernacularnames_csv)
+        for page_id in vernacular_names:
+            name = vernacular_names[page_id]
+            if page_id not in self._scientific:
+                self._scientific[page_id] = name.canonical
+
+            if page_id not in self._common:
+                self._common[page_id] = name.common
+
+        # scientific and taxonomic names
+        for page_id, taxon in parse_all_hierarchies_in_graph(hierarchies_path).items():
+            if page_id not in self._taxonomic:
+                # Sometimes the hierarchies file ignores the species and only has genus
+                # If this is the case, try to find the species from the scientific name
+                if not taxon.species:
+                    scientific = self.scientific(page_id) or ""
+                    split = scientific.split()  # genus, species
+                    if len(split) == 2 and split[0].lower() == taxon.genus.lower():
+                        taxon.species = split[1]
+
+                self._taxonomic[page_id] = taxon.taxonomic
+
+            if page_id not in self._scientific:
+                self._scientific[page_id] = taxon.scientific
+            if page_id not in self._tagged:
+                self._tagged[page_id] = taxon.tagged
+
+        # scientific names
+        with open(scraped_page_ids_csv) as fd:
+            reader = csv.DictReader(fd)
+            for row in tqdm(reader, desc=scraped_page_ids_csv):
+                page_id = int(row["page_id"])
+                name = naming.strip_html(row["scientific_name"])
+
+                if page_id not in self._scientific:
+                    self._scientific[page_id] = name
+
+    def scientific(self, key):
+        if key not in self._scientific:
+            return None
+
+        return self._scientific[key]
+
+    def common(self, key):
+        if key not in self._common:
+            return None
+
+        return self._common[key]
+
+    def taxonomic(self, key):
+        if key not in self._taxonomic:
+            return None
+
+        return self._taxonomic[key]
+
+    def tagged(self, key):
+        if key not in self._tagged:
+            return None
+
+        return self._tagged[key]
+
+    def keys(self):  # list[int]
+        return list(
+            set(self._scientific)
+            | set(self._taxonomic)
+            | set(self._common)
+            | set(self._tagged)
+        )
--- src/imageomics/naming.py
+++ src/imageomics/naming.py
@@ -1,35 +1,10 @@
-"""
-Helpers and utilities for manipulating scientific and common names of species.
-"""
+import csv
 import dataclasses
 import json
+import os
+import re
 
-mapping_file = "data/inat/common-names-mapping.json"
-
-
-def read_mapping():
-    mapping = {}
-    with open(mapping_file) as fd:
-        for key, value in json.load(fd).items():
-            mapping[int(key)] = value
-
-    return mapping
-
-
-def write_mapping(mapping):
-    """
-    Makes sure the mapping is a lookup from dataset id (integer under 10K) to common name
-    """
-    assert isinstance(mapping, dict)
-
-    for key, value in mapping.items():
-        assert isinstance(key, int)
-        assert key < 10000
-        assert isinstance(value, str)
-        assert "_" not in value
-
-    with open(mapping_file, "w") as fd:
-        json.dump(mapping, fd, indent=4)
+from tqdm import tqdm
 
 
 @dataclasses.dataclass
@@ -41,16 +16,19 @@ class Taxon:
     family: str
     genus: str
     species: str
-    # id from inaturalist.org (taxa.csv)
-    website_id: int = -1
-    # id from the inat21 dataset
-    dataset_id: int = -1
-    # common name (from VernacularNames-english.csv)
-    common_name: str = ""
+
+    def __post_init__(self):
+        assert isinstance(self.kingdom, str)
+        assert isinstance(self.phylum, str)
+        assert isinstance(self.cls, str)
+        assert isinstance(self.order, str)
+        assert isinstance(self.family, str)
+        assert isinstance(self.genus, str)
+        assert isinstance(self.species, str)
 
     @property
-    def taxonomic_name(self):
-        return " ".join(
+    def taxonomic(self):
+        name = " ".join(
             [
                 self.kingdom.capitalize(),
                 self.phylum.capitalize(),
@@ -60,14 +38,236 @@ class Taxon:
                 self.genus.capitalize(),
                 self.species.lower(),
             ]
-        )
+        ).strip()
+
+        # Remove double whitespaces
+        return " ".join(name.split())
 
     @property
-    def scientific_name(self):
-        return " ".join([self.genus.capitalize(), self.species.lower()])
+    def scientific(self):
+        name = " ".join(
+            [
+                self.genus.capitalize(),
+                self.species.lower(),
+            ]
+        ).strip()
+
+        # Remove double whitespaces
+        return " ".join(name.split())
+
+    @property
+    def tagged(self):
+        return [
+            (key, value)
+            for key, value in [
+                ("kingdom", self.kingdom.capitalize()),
+                ("phylum", self.phylum.capitalize()),
+                ("class", self.cls.capitalize()),
+                ("order", self.order.capitalize()),
+                ("family", self.family.capitalize()),
+                ("genus", self.genus.capitalize()),
+                ("species", self.species.lower()),
+            ]
+            if value
+        ]
 
 
-def dataset_class_to_taxon(cls):
-    index, *tiers = cls.split("_")
-    index = int(index, base=10)
-    return Taxon(*tiers, dataset_id=index)
+class NameLookup:
+    """Lookup from a key to scientific name, taxonomic name and key name.
+    Meant to be subclassed for each different data source.
+    """
+
+    def scientific(self, key: str):  # str | None
+        raise NotImplementedError()
+
+    def taxonomic(self, key: str):  # str | None
+        raise NotImplementedError()
+
+    def common(self, key: str):  # str | None
+        raise NotImplementedError()
+
+    def tagged(self):  # list[tuple[str, str]]
+        raise NotImplementedError()
+
+    def keys(self):  # list[str]
+        raise NotImplementedError()
+
+
+class BioscanNameLookup(NameLookup):
+    def __init__(
+        self,
+        bioscan_metadata_path="/fs/scratch/PAS2136/bioscan/BIOSCAN_Insect_Dataset_metadata.jsonld",
+    ):
+        self.lookup = {}
+
+        with open(bioscan_metadata_path) as fd:
+            bioscan_metadata = json.load(fd)
+
+        for row in tqdm(bioscan_metadata, desc="Loading Bioscan metadata"):
+            taxon = Taxon(
+                "Animalia",
+                "Arthropoda",
+                "Insecta",
+                row["order"] if row["order"] != "not_classified" else "",
+                row["family"] if row["family"] != "not_classified" else "",
+                row["genus"] if row["genus"] != "not_classified" else "",
+                row["species"] if row["species"] != "not_classified" else "",
+            )
+            self.lookup[row["image_file"]] = taxon
+
+    def scientific(self, key):
+        if key not in self.lookup:
+            return None
+
+        return self.lookup[key].scientific
+
+    def taxonomic(self, key):
+        if key not in self.lookup:
+            return None
+
+        return self.lookup[key].taxonomic
+
+    def tagged(self, key):
+        if key not in self.lookup:
+            return None
+
+        return self.lookup[key].tagged
+
+    def common(self, key):
+        return None
+
+    def keys(self):
+        return list(self.lookup.keys())
+
+
+class iNaturalistNameLookup(NameLookup):
+    def __init__(
+        self,
+        taxa_csv="data/inat/dwca/taxa.csv",
+        vernacularnames_english_csv="data/inat/dwca/VernacularNames-english.csv",
+    ):
+        self._scientific = {}
+        self._taxonomic = {}
+        self._common = {}
+
+        with open(taxa_csv, "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row["taxonRank"] != "species":
+                    continue
+
+                king = row["kingdom"]
+                phyl = row["phylum"]
+                cls = row["class"]
+                ord = row["order"]
+                fam = row["family"]
+                genus = row["genus"]
+                species = row["specificEpithet"]
+
+                scientific = f"{genus.capitalize()} {species.lower()}"
+                self._scientific[int(row["id"])] = scientific
+                self._taxonomic[int(row["id"])] = " ".join(
+                    [
+                        king.capitalize(),
+                        phyl.capitalize(),
+                        cls.capitalize(),
+                        ord.capitalize(),
+                        fam.capitalize(),
+                        genus.capitalize(),
+                        species.lower(),
+                    ]
+                )
+
+        with open(vernacularnames_english_csv, "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self._common[int(row["id"])] = row["vernacularName"]
+
+    def scientific(self, key):
+        if key not in self._scientific:
+            return None
+
+        return self._scientific[key]
+
+    def common(self, key):
+        if key not in self._common:
+            return None
+
+        return self._common[key]
+
+    def taxonomic(self, key):
+        if key not in self._taxonomic:
+            return None
+
+        return self._taxonomic[key]
+
+    def keys(self):  # list[int]
+        return list(set(self._scientific) | set(self._taxonomic) | set(self._common))
+
+
+class iNat21NameLookup(NameLookup):
+    def __init__(self, inat_root="/fs/ess/PAS2136/foundation_model/inat21/raw/train"):
+        self._taxa = {}
+
+        for clsdir in os.listdir(inat_root):
+            index, king, phyl, cls, ord, fam, genus, species = clsdir.split("_")
+            index = int(index, base=10)
+            taxon = Taxon(king, phyl, cls, ord, fam, genus, species)
+            self._taxa[clsdir] = taxon
+
+    def scientific(self, key):
+        if key not in self._taxa:
+            return None
+
+        return self._taxa[key].scientific
+
+    def taxonomic(self, key):
+        if key not in self._taxa:
+            return None
+
+        return self._taxa[key].taxonomic
+
+    def tagged(self, key):
+        if key not in self._taxa:
+            return None
+
+        return self._taxa[key].tagged
+
+    def common(self, key):
+        return None
+
+    def keys(self):  # list[str]
+        return list(self._taxa.keys())
+
+
+def tagged_to_scientific(tagged):
+    genus, species = "", ""
+    for tier, value in tagged:
+        if tier == "genus":
+            genus = value
+        if tier == "species":
+            species = value
+
+    if not genus and not species:
+        # Just use whatever taxonomic information we have.
+        return " ".join(value.capitalize() for tier, value in tagged)
+
+    return f"{genus.capitalize()} {species.lower()}".strip()
+
+
+def taxonomic_to_scientific(taxonomic):
+    tiers = taxonomic.split()
+
+    # Assume that if there are at least 7 "words" in the taxonomic name, then the last
+    # two are likely to be genus and species. But if there are fewer than 7, it's more
+    # likely that it's missing species, genus, family, etc. in that order.
+    if len(tiers) < 7:
+        return ""
+
+    *_, genus, species = tiers
+    return f"{genus.capitalize()} {species.lower()}".strip()
+
+
+def strip_html(string):
+    """Just removes <i>...</i> tags and similar. Not complete."""
+    return re.sub(r"<.*?>(.*?)</.*?>", r"\1", string)
