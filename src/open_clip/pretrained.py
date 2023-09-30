0d66509e3f18249a33103708251a61ab5ef5270f
Mehdi Cherti <mehdicherti@gmail.com>
2023-09-30 07:57:41 +0200

    add vitb32 256x256 datacomp 34B samples seen model

--- src/open_clip/pretrained.py
+++ src/open_clip/pretrained.py
@@ -102,8 +102,6 @@ _VITB32 = dict(
     commonpool_s_text_s13m_b4k=_pcfg(hf_hub='laion/CLIP-ViT-B-32-CommonPool.S.text-s13M-b4K/'),
     commonpool_s_basic_s13m_b4k=_pcfg(hf_hub='laion/CLIP-ViT-B-32-CommonPool.S.basic-s13M-b4K/'),
     commonpool_s_s13m_b4k=_pcfg(hf_hub='laion/CLIP-ViT-B-32-CommonPool.S-s13M-b4K/'),
-    # DataComp other models
-    datacomp_s34b_b86k=_pcfg(hf_hub='laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K'),
 )
 
 _VITB32_quickgelu = dict(
@@ -113,6 +111,10 @@ _VITB32_quickgelu = dict(
         "https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_b_32-quickgelu-laion400m_e31-d867053b.pt"),
     laion400m_e32=_pcfg(
         "https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_b_32-quickgelu-laion400m_e32-46683a32.pt"),
+)
+
+_VITB32_256 = dict(
+    datacomp_s34b_b86k=_pcfg(hf_hub='laion/CLIP-ViT-B-32-256x256-DataComp-s34B-b86K/'),
 )
 
 _VITB16 = dict(
@@ -239,6 +241,7 @@ _PRETRAINED = {
     "RN50x16": _RN50x16,
     "RN50x64": _RN50x64,
     "ViT-B-32": _VITB32,
+    "ViT-B-32-256": _VITB32_256,
     "ViT-B-32-quickgelu": _VITB32_quickgelu,
     "ViT-B-16": _VITB16,
     "ViT-B-16-plus-240": _VITB16_PLUS_240,
