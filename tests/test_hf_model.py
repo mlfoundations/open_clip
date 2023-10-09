0142d279298a4ca0138316286f775fe9d7bdbb94
Seyed Alireza Fatemi Jahromi <alirezafa@rocketmail.com>
2023-10-09 18:38:08 +0300

    use bool masks (#665)

--- tests/test_hf_model.py
+++ tests/test_hf_model.py
@@ -8,8 +8,8 @@ from transformers.modeling_outputs import BaseModelOutput
 def test_poolers():
     bs, sl, d = 2, 10, 5
     h = torch.arange(sl).repeat(bs).reshape(bs, sl)[..., None] * torch.linspace(0.2, 1., d)
-    mask = torch.ones(bs, sl, dtype=torch.long)
-    mask[:2, 6:] = 0
+    mask = torch.ones(bs, sl, dtype=torch.bool)
+    mask[:2, 6:] = False
     x = BaseModelOutput(h)
     for name, cls in _POOLERS.items():
         pooler = cls()
