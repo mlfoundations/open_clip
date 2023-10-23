c4aeed889c7d44c6578377189b8db87cbf3fbf6e
Gabriel Ilharco <gabriel.ilharco@gmail.com>
2023-10-23 13:43:00 +0000

    Add torch flop counter

--- src/training/profiler.py
+++ src/training/profiler.py
@@ -4,7 +4,7 @@ import torch
 import open_clip
 import pandas as pd
 from fvcore.nn import FlopCountAnalysis, flop_count_str, ActivationCountAnalysis
-
+from torch.utils.flop_counter import FlopCounterMode
 
 parser = argparse.ArgumentParser(description='OpenCLIP Profiler')
 
@@ -13,6 +13,8 @@ parser.add_argument('--model', metavar='NAME', default='',
                     help='model(s) to profile')
 parser.add_argument('--results-file', default='', type=str, metavar='FILENAME',
                     help='Output csv file for results')
+parser.add_argument('--profiler', default='torch', type=str, choices=['torch', 'fvcore'])
+parser.add_argument('--batch-size', default=1, type=int, help='Batch size for profiling')
 
 
 def profile_fvcore(
@@ -33,7 +35,7 @@ def profile_fvcore(
     if detailed:
         fcs = flop_count_str(fca)
         print(fcs)
-    return fca.total(), aca.total()
+    return fca.total() / batch_size, aca.total() / batch_size
 
 
 def profile_fvcore_text(
@@ -52,7 +54,7 @@ def profile_fvcore_text(
     if detailed:
         fcs = flop_count_str(fca)
         print(fcs)
-    return fca.total(), aca.total()
+    return fca.total() / batch_size, aca.total() / batch_size
 
 
 def profile_fvcore_image(
@@ -71,14 +73,57 @@ def profile_fvcore_image(
     if detailed:
         fcs = flop_count_str(fca)
         print(fcs)
-    return fca.total(), aca.total()
+    return fca.total() / batch_size, aca.total() / batch_size
+
+
+def profile_torch_image(model, image_input_size, batch_size=1, force_cpu=False):
+    """Profile the image encoder using torch.utils.flop_counter"""
+    if force_cpu:
+        model = model.to('cpu')
+    device, dtype = next(model.parameters()).device, next(model.parameters()).dtype
+    example_input = torch.ones((batch_size,) + image_input_size, device=device, dtype=dtype)
+
+    flop_counter = FlopCounterMode(model)
+    with flop_counter:
+        model(example_input)
+    total_flops = sum(flop_counter.get_flop_counts()['Global'].values())
+    return total_flops / batch_size
+
+
+def profile_torch_text(model, text_input_size, batch_size=1, force_cpu=False):
+    """Profile the text encoder using torch.utils.flop_counter"""
+    if force_cpu:
+        model = model.to('cpu')
+    device = next(model.parameters()).device
+    example_input = torch.ones((batch_size,) + text_input_size, device=device, dtype=torch.int64)
+
+    flop_counter = FlopCounterMode(model)
+    with flop_counter:
+        model(example_input)
+    total_flops = sum(flop_counter.get_flop_counts()['Global'].values())
+    return total_flops / batch_size
+
+
+def profile_torch(model, text_input_size, image_input_size, batch_size=1, force_cpu=False):
+    """Profile the full model using torch.utils.flop_counter"""
+    if force_cpu:
+        model = model.to('cpu')
+    device, dtype = next(model.parameters()).device, next(model.parameters()).dtype
+    image_input = torch.ones((batch_size,) + image_input_size, device=device, dtype=dtype)
+    text_input = torch.ones((batch_size,) + text_input_size, device=device, dtype=torch.int64)
+
+    flop_counter = FlopCounterMode(model)
+    with flop_counter:
+        model(image_input, text_input)
+    total_flops = sum(flop_counter.get_flop_counts()['Global'].values())
+    return total_flops / batch_size
 
 
 def count_params(model):
     return sum([m.numel() for m in model.parameters()])
 
-
-def profile_model(model_name):
+def profile_model(model_name, batch_size=1, profiler='torch'):
+    assert profiler in ['torch', 'fvcore'], 'Only torch and fvcore profilers are supported'
     model = open_clip.create_model(model_name, force_custom_text=True, pretrained_hf=False)
     model.eval()
     if torch.cuda.is_available():
@@ -88,7 +133,7 @@ def profile_model(model_name):
         image_input_size = (3,) + tuple(model.visual.image_size[-2:])
     else:
         image_input_size = (3, model.visual.image_size, model.visual.image_size)
-    text_input_size = (77,)
+    text_input_size = (model.context_length,) if hasattr(model, 'context_length') else (77,)
 
     results = {}
     results['model'] = model_name
@@ -110,24 +155,40 @@ def profile_model(model_name):
     while retries:
         retries -= 1
         try:
-            macs, acts = profile_fvcore(
-                model, image_input_size=image_input_size, text_input_size=text_input_size, force_cpu=not retries)
-
-            image_macs, image_acts = profile_fvcore_image(
-                model.visual, image_input_size=image_input_size, force_cpu=not retries)
-
-            text_macs, text_acts = profile_fvcore_text(
-                model.text, text_input_size=text_input_size, force_cpu=not retries)
-
-            results['gmacs'] = round(macs / 1e9, 2)
-            results['macts'] = round(acts / 1e6, 2)
             results['mparams'] = round(count_params(model) / 1e6, 2)
-            results['image_gmacs'] = round(image_macs / 1e9, 2)
-            results['image_macts'] = round(image_acts / 1e6, 2)
             results['image_mparams'] = round(count_params(model.visual) / 1e6, 2)
-            results['text_gmacs'] = round(text_macs / 1e9, 2)
-            results['text_macts'] = round(text_acts / 1e6, 2)
             results['text_mparams'] = round(count_params(model.text) / 1e6, 2)
+
+            if profiler == 'fvcore':
+                macs, acts = profile_fvcore(
+                    model, image_input_size=image_input_size, text_input_size=text_input_size, force_cpu=not retries, batch_size=batch_size)
+
+                image_macs, image_acts = profile_fvcore_image(
+                    model.visual, image_input_size=image_input_size, force_cpu=not retries, batch_size=batch_size)
+
+                text_macs, text_acts = profile_fvcore_text(
+                    model.text, text_input_size=text_input_size, force_cpu=not retries, batch_size=batch_size)
+
+                results['gmacs'] = round(macs / 1e9, 2)
+                results['macts'] = round(acts / 1e6, 2)
+                
+                results['image_gmacs'] = round(image_macs / 1e9, 2)
+                results['image_macts'] = round(image_acts / 1e6, 2)
+                
+                results['text_gmacs'] = round(text_macs / 1e9, 2)
+                results['text_macts'] = round(text_acts / 1e6, 2)
+            elif profiler == 'torch':
+                image_flops = profile_torch_image(
+                    model.visual, image_input_size=image_input_size, force_cpu=not retries, batch_size=batch_size)
+                text_flops = profile_torch_text(
+                    model.text, text_input_size=text_input_size, force_cpu=not retries, batch_size=batch_size)
+                total_flops = profile_torch(
+                    model, image_input_size=image_input_size, text_input_size=text_input_size, force_cpu=not retries, batch_size=batch_size)
+
+                results['gflops'] = round(total_flops / 1e9, 2)
+                results['image_gflops'] = round(image_flops / 1e9, 2)
+                results['text_gflops'] = round(text_flops / 1e9, 2)
+
         except RuntimeError as e:
             pass
     return results
@@ -143,15 +204,32 @@ def main():
         parsed_model = args.model.split(',')
 
     results = []
+    models_with_errors = []
     for m in parsed_model:
-        row = profile_model(m)
-        results.append(row)
+        print('='*100)
+        print(f'Profiling {m}')
+        try:
+            row = profile_model(m, batch_size=args.batch_size, profiler=args.profiler)
+            results.append(row)
+        except Exception as e:
+            print(f'Error profiling {m}: {e}')
+            import traceback
+            traceback.print_exc()
+            models_with_errors.append(m)
 
     df = pd.DataFrame(results, columns=results[0].keys())
-    df = df.sort_values('gmacs')
+    if 'gmacs' in df.columns:
+        df = df.sort_values('gmacs')
+    else:
+        df = df.sort_values('gflops')
+
+    print('='*100)
+    print('Done.')
     print(df)
     if args.results_file:
         df.to_csv(args.results_file, index=False)
+
+    print('Models with errors:', models_with_errors)
 
 
 if __name__ == '__main__':
