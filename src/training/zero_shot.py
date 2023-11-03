acd5af79cc2b8498383d84d8771c43cf8a5470b8
Mehdi Cherti <m.cherti@fz-juelich.de>
2023-11-03 14:11:14 +0100

    remove old zero shot classifier builder

--- src/training/zero_shot.py
+++ src/training/zero_shot.py
@@ -8,27 +8,6 @@ from open_clip import get_input_dtype, get_tokenizer, build_zero_shot_classifier
 from .precision import get_autocast
 
 
-def zero_shot_classifier(model, classnames, templates, args):
-    tokenizer = get_tokenizer(args.model)
-    with torch.no_grad():
-        zeroshot_weights = []
-        for classname in tqdm(classnames):
-            texts = [template(classname) for template in templates]  # format with class
-            texts = tokenizer(texts).to(args.device)  # tokenize
-            if args.distributed and not args.horovod:
-                if args.fsdp:
-                    _, class_embeddings, _ = model(image=None, text=texts)
-                else:
-                    class_embeddings = model.module.encode_text(texts)
-            else:
-                class_embeddings = model.encode_text(texts)
-            class_embedding = F.normalize(class_embeddings, dim=-1).mean(dim=0)
-            class_embedding /= class_embedding.norm()
-            zeroshot_weights.append(class_embedding)
-        zeroshot_weights = torch.stack(zeroshot_weights, dim=1).to(args.device)
-    return zeroshot_weights
-
-
 def accuracy(output, target, topk=(1,)):
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
