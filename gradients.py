b3d750269fc1cb57620943cfe4df0085105e48c0
martin.preiss <martinpreiss524@gmail.com>
2023-11-23 15:51:49 +0700

    vit experimental stand 20.11.2023

--- gradients.py
+++ gradients.py
@@ -13,6 +13,7 @@ import torch
 import torch.nn as nn
 from PIL import Image
 from src.gradcam.hook import Hook
+from open_clip.transformer import VisionTransformer
 from src.gradcam.utils import get_cnn_modules,show_attention_map
 
 
@@ -27,6 +28,47 @@ def reshape_transform(tensor, height=14, width=14):
     result = result.transpose(2, 3).transpose(1, 2)
     return result
 
+def grad_cam_test(model,layer,image,text,targets,target_index,idx):
+    requires_grad = {}
+    for name, param in model.visual.named_parameters():
+        requires_grad[name] = param.requires_grad
+        param.requires_grad_(False)
+    with torch.cuda.amp.autocast(), torch.autograd.set_detect_anomaly(True), torch.set_grad_enabled(True), Hook(layer) as hook:
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+        normalized_image_features = image_features / torch.linalg.norm(image_features,dim=-1, keepdim=True)
+        normalized_text_features = text_features / torch.linalg.norm(text_features,dim=-1, keepdim=True)
+        text_probs = (100.0 * normalized_image_features @ normalized_text_features.T)
+        #output = model.visual(image)
+        #output.backward(model.encode_text(text))
+        #text_probs.backward(torch.tensor([[1,0]]))
+        text_probs[:,target_index].backward()
+        #extract from layer 
+        grad = hook.gradient.float()
+        act = hook.activation.float()
+        #print("image_features:",image_features.shape)
+        #print("act:",act.shape)
+        #print("grad:",grad.shape)
+        if isinstance(model.visual, VisionTransformer):
+            grad = reshape_transform(grad)
+            act = reshape_transform(act)
+        #print(act)
+        #print("act:",act.shape)
+        #print("grad:",grad.shape)
+        #print(grad)
+        global_avg_pooled = grad.mean(dim=(2,3),keepdim=True)
+        wheigted_act_sum = torch.sum(act*global_avg_pooled,dim=1,keepdim=True)
+        #transform(wheigted_act_sum[0]).show()
+        grad_cam = torch.clamp(wheigted_act_sum,min=0)
+        #transform(grad_cam[0]).show()
+        #print("max:",torch.max(grad_cam))    
+        grad_cam /= torch.max(grad_cam)
+        #transform(grad_cam[0]).show()
+        grad_cam = grad_cam.squeeze().detach().cpu().numpy()
+        ##print(grad_cam)
+        ##print(grad_cam.shape)
+        show_attention_map(grad_cam,image_name,layer_name=str(layer),caption=targets[target_index],write_to_disk=True,idx=idx)
+
 image_name = "test.jpg"
 
 model_name = "ViT-B-16"
@@ -35,81 +77,52 @@ pretrain_tag = "openai"
 #model_name = "convnext_base"
 #pretrain_tag = "laion400m_s13b_b51k"
 
+#model_name = "RN50"
+#pretrain_tag = "openai"
+
 model, _, preprocess = open_clip.create_model_and_transforms(model_name, pretrained=pretrain_tag)
 tokenizer = open_clip.get_tokenizer(model_name)
 
 image = preprocess(Image.open("test.jpg")).unsqueeze(0)
 
-print(image.shape)
+#print(image.shape)
 transform = T.ToPILImage()
-
-text = tokenizer(["a cat", "a dog"])
+targets= ["a cat", "a dog"]
+text = tokenizer(targets)
+target_index= 1
 
 #cnn_modules= get_cnn_modules(model.visual)
-#print(model)
+##print(model)
 #last_layer = cnn_modules[1]
-#print(last_layer
+##print(last_layer
 
 
 #if image.grad is not None:
 #    image.grad.data.zero_()
-layer = model.visual.transformer.resblocks[-2].ls_2
+#layer = model.visual.transformer.resblocks[-2].ls_2
 
 
-#print(model)
+##print(model)
 
-requires_grad = {}
-for name, param in model.visual.named_parameters():
-    requires_grad[name] = param.requires_grad
-    param.requires_grad_(False)
 
-with torch.cuda.amp.autocast(), torch.autograd.set_detect_anomaly(True), torch.set_grad_enabled(True), Hook(layer) as hook:
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
-    
-    normalized_image_features = image_features / torch.linalg.norm(image_features,dim=-1, keepdim=True)
-    normalized_text_features = text_features / torch.linalg.norm(text_features,dim=-1, keepdim=True)
-    text_probs = (100.0 * normalized_image_features @ normalized_text_features.T)
-    #output = model.visual(image)
-    #output.backward(model.encode_text(text))
-    #text_probs.backward(torch.tensor([[1,0]]))
-    text_probs[:,1].backward()
-    
-    #extract from layer 
-    grad = hook.gradient.float()
-    act = hook.activation.float()
-    print("image_features:",image_features.shape)
-    print("act:",act.shape)
-    print("grad:",grad.shape)
 
-    grad = reshape_transform(grad)
-    act = reshape_transform(act)
-    print(act)
-    print("act:",act.shape)
+from src.gradcam.utils import get_all_layers
+layers = get_all_layers(model.visual,[])
+#layers = model.visual.transformer.resblocks
+print(len(layers))
+#layer = model.visual.trunk.stages[-1]
+#grad_cam_test(model,layer,image,text,targets,target_index,idx=0)
 
-    print("grad:",grad.shape)
-    
-    print(grad)
+for idx,layer in enumerate(layers):
+    try:
+        grad_cam_test(model,layer,image,text,targets,target_index,idx=str(idx))
+    except Exception as e:
+        print(e)
+        print("did not work for layer:",layer)
 
-    global_avg_pooled = grad.mean(dim=(2,3),keepdim=True)
-    
-    wheigted_act_sum = torch.sum(act*global_avg_pooled,dim=1,keepdim=True)
-    #transform(wheigted_act_sum[0]).show()
-    grad_cam = torch.clamp(wheigted_act_sum,min=0)
-    #transform(grad_cam[0]).show()
-    print("max:",torch.max(grad_cam))    
-    grad_cam /= torch.max(grad_cam)
 
-    #transform(grad_cam[0]).show()
-    grad_cam = grad_cam.squeeze().detach().cpu().numpy()
 
-    #print(grad_cam)
+#print("Label probs:", text_probs)  # #prints: [[1., 0., 0.]]
 
-    #print(grad_cam.shape)
-
-    show_attention_map(grad_cam,image_name)
-
-print("Label probs:", text_probs)  # prints: [[1., 0., 0.]]
-
-#print(model.visual.trunk.stages[-1])
-#print(len(cnn_modules))
+##print(model.visual.trunk.stages[-1])
+##print(len(cnn_modules))
--- src/gradcam/utils.py
+++ src/gradcam/utils.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 import numpy as np
-
+import os
 from PIL import Image
 from torch import nn
 
@@ -12,10 +12,17 @@ def load_image(img_path, resize=None):
         image = image.resize((resize, resize))
     return np.asarray(image).astype(np.float32) / 255.0
 
+def check_and_create_path(path):
+    # Check if the path exists
+    if not os.path.exists(path):
+        # If the path doesn't exist, create it
+        os.makedirs(path)
+        print(f"Path '{path}' created.")
 
-def show_attention_map(heatmap, image_name:str, layer_name:str="",write_to_disk:bool=False):
-    _, axes = plt.subplots(1, 3, figsize=(15, 8))
-    axes[0].matshow(heatmap.squeeze())
+def show_attention_map(heatmap, image_name:str, layer_name:str="",caption:str="",write_to_disk:bool=False,idx:str=False):
+    _, axes = plt.subplots(1, 1, figsize=(15, 8))
+    axes.matshow(heatmap.squeeze())
+    '''
     img = cv2.imread(image_name)
     heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
     heatmap = np.uint8(255 * heatmap)
@@ -23,11 +30,19 @@ def show_attention_map(heatmap, image_name:str, layer_name:str="",write_to_disk:
     result_img = (heatmap * 0.4 + img).astype(np.float32)
     axes[1].imshow(img[..., ::-1])
     axes[2].imshow((result_img / 255)[..., ::-1])
-    for ax in axes:
-        ax.axis("off")
-    plt.show()
+    '''
+    #for ax in axes:
+    #    ax.axis("off")
     if write_to_disk:
-        plt.savefig('/imgs/'+layer_name+".jpg")
+        cwd =  os.getcwd()
+        dict_path = cwd + '/imgs/heatmaps/'+caption+"/"
+        check_and_create_path(dict_path)
+        img_name = idx+layer_name+".jpg"
+        path = dict_path+img_name
+        plt.savefig(path)
+
+    else:
+        plt.show()
 
         
 def get_cnn_modules(module,cnn_module_list=[]):
@@ -37,3 +52,11 @@ def get_cnn_modules(module,cnn_module_list=[]):
         elif child.children() is not None:
             cnn_module_list = get_cnn_modules(child,cnn_module_list)
     return cnn_module_list
+
+def get_all_layers(module,layer_list=[]):
+    for child in module.children():
+        if len(list(child.children()))==0:
+            layer_list.append(child)
+        else:
+            layer_list = get_all_layers(child,layer_list)
+    return layer_list
