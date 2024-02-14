fd8946190c3b4e769f869e98a89178dba5c17e72
George Mastrapas <georgios.mastrapas@jina.ai>
2024-02-14 15:01:31 +0200

    feat: tie projection layers in three towers

--- src/open_clip/multi_tower_model.py
+++ src/open_clip/multi_tower_model.py
@@ -3,13 +3,15 @@ from typing import Optional, Union
 import numpy as np
 import torch
 import torch.nn.functional as f
-from torch import nn
 
-from .model import _build_text_tower, _build_vision_tower, CLIPTextCfg, CLIPVisionCfg
+from .hf_model import HFTextEncoder
+from .transformer import VisionTransformer, TextTransformer
+from .model import (
+    _build_text_tower, _build_vision_tower, CLIPTextCfg, CLIPVisionCfg, CustomTextCLIP
+)
 
 
-class ThreeTowerCustomTextCLIP(nn.Module):
-    output_dict: torch.jit.Final[bool]
+class ThreeTowersCustomTextCLIP(CustomTextCLIP):
 
     def __init__(
         self,
@@ -23,10 +25,16 @@ class ThreeTowerCustomTextCLIP(nn.Module):
         cast_dtype: Optional[torch.dtype] = None,
         output_dict: bool = False,
     ):
-        super().__init__()
-        self.output_dict = output_dict
-        self.visual = _build_vision_tower(embed_dim, vision_cfg, quick_gelu, cast_dtype)
-        self.text = _build_text_tower(embed_dim, text_cfg, quick_gelu, cast_dtype)
+        super(ThreeTowersCustomTextCLIP, self).__init__(
+            embed_dim=embed_dim,
+            vision_cfg=vision_cfg,
+            text_cfg=text_cfg,
+            quick_gelu=quick_gelu,
+            init_logit_scale=init_logit_scale,
+            init_logit_bias=init_logit_bias,
+            cast_dtype=cast_dtype,
+            output_dict=output_dict,
+        )
         if 'hf_tokenizer_name' in teacher_cfg:
             self.teacher = _build_text_tower(
                 embed_dim, teacher_cfg, quick_gelu, cast_dtype
@@ -39,70 +47,69 @@ class ThreeTowerCustomTextCLIP(nn.Module):
             )
             self.teacher.lock(unlocked_groups=0, freeze_bn_stats=False)
             self.teacher_type = 'vision'
-        self.context_length = self.text.context_length
-        self.vocab_size = self.text.vocab_size
-        self.logit_scale = nn.Parameter(torch.ones([]) * init_logit_scale)
-        if init_logit_bias is not None:
-            self.logit_bias = nn.Parameter(torch.ones([]) * init_logit_bias)
+
+        self._tie_projections()
+
+    def _tie_projections(self):
+        if self.teacher_type == 'text':
+            assert type(self.teacher) is type(self.text), (
+                'Unable to tie the projections layers, '
+                f'teacher model is of type `{type(self.teacher)}` while text '
+                f'model is of type `{type(self.text)}`'
+            )
+            if isinstance(self.teacher, TextTransformer):
+                self.teacher.text_projection.weight = self.text.text_projection.weight
+                if (
+                    hasattr(self.teacher.text_projection, 'bias')
+                    and hasattr(self.text.text_projection, 'bias')
+                ):
+                    self.teacher.text_projection.bias = self.text.text_projection.bias
+            elif isinstance(self.teacher, HFTextEncoder):
+                # TODO: This will fail for MLP or Identity projection
+                self.teacher.proj.weight = self.text.proj.weight
+            else:
+                raise TypeError(
+                    f'Teacher model type `{type(self.teacher)}` is not '
+                    f'compatible with the 3towers architecture!'
+                )
         else:
-            self.logit_bias = None
+            assert type(self.teacher) is type(self.visual), (
+                'Unable to tie the projections layers, '
+                f'teacher model is of type `{type(self.teacher)}` while vision '
+                f'model is of type `{type(self.visual)}`'
+            )
+            if isinstance(self.teacher, VisionTransformer):
+                self.teacher.proj.weight = self.visual.proj.weight
+            else:
+                raise TypeError(
+                    f'Teacher model type `{type(self.teacher)}` is not '
+                    f'compatible with the 3towers architecture!'
+                )
 
-    def lock_image_tower(self, unlocked_groups=0, freeze_bn_stats=False):
-        # lock image tower as per LiT - https://arxiv.org/abs/2111.07991
-        self.visual.lock(
-            unlocked_groups=unlocked_groups, freeze_bn_stats=freeze_bn_stats
-        )
-
-    def lock_text_tower(self, unlocked_layers: int = 0, freeze_layer_norm: bool = True):
-        self.text.lock(unlocked_layers, freeze_layer_norm)
-
-    @torch.jit.ignore
-    def set_grad_checkpointing(self, enable=True):
-        self.visual.set_grad_checkpointing(enable)
-        self.text.set_grad_checkpointing(enable)
-
-    def encode_image(self, image, normalize: bool = False):
-        features = self.visual(image)
+    def encode_teacher(self, image_or_text, normalize: bool = False):
+        features = self.teacher(image_or_text)
         return f.normalize(features, dim=-1) if normalize else features
-
-    def encode_text(self, text, normalize: bool = False):
-        features = self.text(text)
-        return f.normalize(features, dim=-1) if normalize else features
-
-    def encode_teacher(self, image, normalize: bool = False):
-        features = self.teacher(image)
-        return f.normalize(features, dim=-1) if normalize else features
-
-    # def get_logits(self, image, text):
-    #     image_features = self.encode_image(image, normalize=True)
-    #     text_features = self.encode_text(text, normalize=True)
-    #     image_logits = self.logit_scale.exp() * image_features @ text_features.T
-    #     if self.logit_bias is not None:
-    #         image_logits += self.logit_bias
-    #     text_logits = image_logits.T
-    #     return image_logits, text_logits
 
     def forward(
-        self,
-        image: Optional[torch.Tensor] = None,
-        text: Optional[torch.Tensor] = None,
+        self, image: Optional[torch.Tensor] = None, text: Optional[torch.Tensor] = None,
     ):
         image_features = (
-            self.encode_image(image, normalize=True) if image is not None else None
+            self.encode_image(image, normalize=True)
+            if image is not None else None
         )
         text_features = (
-            self.encode_text(text, normalize=True) if text is not None else None
+            self.encode_text(text, normalize=True)
+            if text is not None else None
         )
         if self.teacher_type == 'vision':
-            teacher_features = (
-                self.encode_teacher(image, normalize=True)
-                if image is not None else None
-            )
+            _teacher_input = image
         else:
-            teacher_features = (
-                self.encode_text(text, normalize=True) if text is not None else None
-            )
+            _teacher_input = text
 
+        teacher_features = (
+            self.encode_teacher(_teacher_input, normalize=True)
+            if _teacher_input is not None else None
+        )
         if self.output_dict:
             out_dict = {
                 "image_features": image_features,
