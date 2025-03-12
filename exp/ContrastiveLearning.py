87785c68e40c4fd7ddd2c4ba0cae5c2cdaaf91f9
ziiihoooYe <ziiihooo.ye@gmail.com>
2025-03-12 10:56:54 -0400

    update

--- exp/ContrastiveLearning.py
+++ exp/ContrastiveLearning.py
@@ -1,5 +1,9 @@
+from loss.mse import *
 
 def ContrastiveLearning(img_enc, text_enc, dataset, preprocessor_list, config):
+    
+    # you can load your training config here
+    # {'epochs': 10, 'batch_size': 32, ...}
     train_config = config["train"]
     
     print("Contrastive Learning")
